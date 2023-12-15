4ca655053954be3ad213402f8ef4086b44364838
sureshbhanuka <108489282+sureshbhanuka@users.noreply.github.com>
2023-12-15 13:48:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Blockchain Engineer", "UX/UI Developer", "Crypto Trader"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
